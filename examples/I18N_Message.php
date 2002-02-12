6d28b9939e4403def379a98838d9340a2f89284f
Wolfram Kriesing <cain@php.net>
2002-02-12 17:29:09 +0000

    - example, for showing how it works (i still dont know if examples are ok here)
    git-svn-id: http://svn.php.net/repository/pear/packages/I18N/trunk@70106 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/I18N_Message.php
@@ -0,0 +1,41 @@
+<?php
+
+    /**
+    *
+    *   test of Message_Common
+    *
+    */
+    print 'TEST I18N_Messages_Common<br><br>';
+    require_once( 'I18N/Messages/Common.php' );
+
+    $strings[] = 'This is a test for all of you out there';
+    $strings[] = 'Das ist ein Test für euch alle da draussen.';
+    $strings[] = 'Esto es una prueba para todos vosotros ahi.';
+
+    foreach( $strings as $aString )
+        print "<b>$aString</b> --- seems to be of the language: ".I18N_Messages_Common::get($aString).'<br>';
+
+
+    /**
+    *
+    *   test of Message_Translate
+    *
+    *   DB file: http://wolfram.kriesing.de/libs/php/examples/SimpleTemplate/translate.sql
+    *
+    */
+    print '<br><br>TEST I18N_Messages_Translate<br><br>';
+    require_once( 'I18N/Messages/Translate.php' );
+
+    $translate = new I18N_Messages_Translate( 'mysql://root@localhost/test' );
+
+    $translated[] = $translate->simpleTranslate( 'source code' , 'de' );
+    $translated[] = $translate->translate( 'translate (i.e. into ($someVar-or any other string))' , 'de' );
+    $translated[] = $translate->translateMarkUpString( $input , 'de' );
+
+    foreach( $translated as $aTrans )
+        print "$aTrans<br>";
+
+    print_r( $translate->getAll( 'de' ) );
+
+
+?>

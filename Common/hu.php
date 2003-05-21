8b88161c8c38f8bc8bea1d2a0161010a0a745af6
Wolfram Kriesing <cain@php.net>
2003-05-21 10:45:16 +0000

    - contained strange characters :-/
    git-svn-id: http://svn.php.net/repository/pear/packages/I18N/trunk@128187 c90b9560-bf6c-de11-be94-00142212c4b1

--- Common/hu.php
+++ Common/hu.php
@@ -20,55 +20,55 @@
 class I18N_Common_hu
 {
 
-    var $days = array( 'vasárnap', 'hétfõ', 'kedd', 'szerda', 'csütörtök','péntek', 'szombat' );
+    var $days = array( 'vasárnap', 'hétfõ', 'kedd', 'szerda', 'csütörtök', 'péntek', 'szombat');
+    
+    var $daysAbbreviated = array( 'V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo' );
 
-    var $daysAbbreviated = array( 'V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo' );
+    var $monthsAbbreviated = array( 'jan', 'feb', 'márc', 'ápr', 'máj','jún', 'júl', 'aug', 'szept', 'okt', 'nov', 'dec' );
 
-    var $monthsAbbreviated = array( 'jan', 'feb', 'márc', 'ápr', 'máj','jún', 'júl', 'aug', 'szept', 'okt', 'nov', 'dec' );
+    var $months = array(
+                            'január',
+                            'február',
+                            'március',
+                            'április',
+                            'május',
+                            'június',
+                            'július',
+                            'augusztus',
+                            'szeptember',
+                            'október',
+                            'november',
+                            'december'
+                        );
 
-    var $months = array(
-                            'január',
-                            'február',
-                            'március',
-                            'április',
-                            'május',
-                            'június',
-                            'július',
-                            'augusztus',
-                            'szeptember',
-                            'október',
-                            'november',
-                            'december'
-                         );
+    var $dateFormats = array(
+                            I18N_DATETIME_SHORT     =>  'Y-m-d',
+                            I18N_DATETIME_DEFAULT   =>  'Y. M. d.',
+                            I18N_DATETIME_MEDIUM    =>  'Y. M. d.',
+                            I18N_DATETIME_LONG      =>  'Y. F d.',
+                            I18N_DATETIME_FULL      =>  'Y. F d., l'
+                        );
+    var $timeFormats = array(
+                            I18N_DATETIME_SHORT     =>  'H:i',
+                            I18N_DATETIME_DEFAULT   =>  'H:i:s',
+                            I18N_DATETIME_MEDIUM    =>  'H:i:s',
+                            I18N_DATETIME_LONG      =>  'H:i:s T O',
+                            I18N_DATETIME_FULL      =>  '\i\d\õ: H:i T O'
+                        );
 
-    var $dateFormats = array(
-                            I18N_DATETIME_SHORT     =>  'Y-m-d',
-                            I18N_DATETIME_DEFAULT   =>  'Y. M. d.',
-                            I18N_DATETIME_MEDIUM    =>  'Y. M. d.',
-                            I18N_DATETIME_LONG      =>  'Y. F d.',
-                            I18N_DATETIME_FULL      =>  'Y. F d., l'
-                        );
-    var $timeFormats = array(
-                            I18N_DATETIME_SHORT     =>  'H:i',
-                            I18N_DATETIME_DEFAULT   =>  'H:i:s',
-                            I18N_DATETIME_MEDIUM    =>  'H:i:s',
-                            I18N_DATETIME_LONG      =>  'H:i:s T O',
-                            I18N_DATETIME_FULL      =>  '\i\d\õ: H:i T O'
-                        );
-                   
-   /**
-    * the NUMBER stuff
-    * @var    array   the same parameters as they have to be passed to the number_format-funciton
-    */
-    var $numberFormat = array(
-                                I18N_NUMBER_FLOAT   =>  array('3',',','.'),
-                                I18N_NUMBER_INTEGER =>  array('0',',','.'),
-                            );
+    /**
+    * the NUMBER stuff
+    * @var    array   the same parameters as they have to be passed to the number_format-funciton
+    */
+    var $numberFormat = array(
+                                I18N_NUMBER_FLOAT   =>  array('3',',','.'),
+                                I18N_NUMBER_INTEGER =>  array('0',',','.'),
+                            );
 
-    var $currencyFormats =  array(
-                                    I18N_CURRENCY_LOCAL         =>  array("% Ft" ,    '2' , ',' , '.' ),
-                                    I18N_CURRENCY_INTERNATIONAL =>  array('HUF %' , '2' , '.' , ',' ),
-                                );
-                
+    var $currencyFormats = array(
+                                I18N_CURRENCY_LOCAL         =>  array("% Ft" , '2', ',', '.'),
+                                I18N_CURRENCY_INTERNATIONAL =>  array('HUF %', '2', '.', ',')
+                            );
+
 }
 ?>
